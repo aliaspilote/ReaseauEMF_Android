f9bbf178563f0cfb5af8135237fe61a85c36eac6
Omar Latreche <latreche.omar@gmail.com>
2015-11-02 22:14:24 +0100

    try

---
+++ app/src/main/java/com/emf_asso/bdd/reseauetudiantsmuslmansdefrance/FormBodyManager.java
@@ -0,0 +1,18 @@
+package com.emf_asso.bdd.reseauetudiantsmuslmansdefrance;
+
+import com.squareup.okhttp.FormEncodingBuilder;
+import com.squareup.okhttp.RequestBody;
+
+/**
+ * Created by Omar_Desk on 02/11/2015.
+ */
+public class FormBodyManager {
+
+    public static RequestBody checkmail(String mail) {
+        RequestBody formBody = new FormEncodingBuilder()
+                .add("action", "check_mail")
+                .add("mail", mail)
+                .build();
+        return formBody;
+    }
+}
---
+++ app/src/main/java/com/emf_asso/bdd/reseauetudiantsmuslmansdefrance/HttpReponse.java
@@ -0,0 +1,71 @@
+package com.emf_asso.bdd.reseauetudiantsmuslmansdefrance;
+
+import org.joda.time.DateTime;
+import org.json.simple.JSONObject;
+
+/**
+ * Created by Omar_Desk on 02/11/2015.
+ */
+public class HttpReponse {
+    JSONObject Resultat;
+    Boolean Succes;
+    String Action;
+    DateTime DataReponse;
+    String ExceptionText = "null";
+
+    public HttpReponse() {
+
+    }
+
+    public HttpReponse(Boolean isSucces, String Error) {
+        Succes = isSucces;
+        ExceptionText = Error;
+    }
+
+    public HttpReponse(JSONObject JSONobj, Boolean isSucces, String actionName, DateTime theDate) {
+        Resultat = JSONobj;
+        Succes = isSucces;
+        Action = actionName;
+        DataReponse = theDate;
+    }
+
+    public JSONObject getResultat() {
+        return Resultat;
+    }
+
+    public void setResultat(JSONObject resultat) {
+        Resultat = resultat;
+    }
+
+    public Boolean getSucces() {
+        return Succes;
+    }
+
+    public void setSucces(Boolean succes) {
+        Succes = succes;
+    }
+
+    public String getAction() {
+        return Action;
+    }
+
+    public void setAction(String action) {
+        Action = action;
+    }
+
+    public DateTime getDataReponse() {
+        return DataReponse;
+    }
+
+    public void setDataReponse(DateTime dataReponse) {
+        DataReponse = dataReponse;
+    }
+
+    public String getExceptionText() {
+        return ExceptionText;
+    }
+
+    public void setExceptionText(String exceptionText) {
+        ExceptionText = exceptionText;
+    }
+}

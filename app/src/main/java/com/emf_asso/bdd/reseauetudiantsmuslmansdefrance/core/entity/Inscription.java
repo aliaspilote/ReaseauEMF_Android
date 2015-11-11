3d68ddca236d15bbb1c1be0e3a4801725beb57e9
Omar Latreche <latreche.omar@gmail.com>
2015-11-11 19:59:14 +0100

    Entitées ajouter

--- app/src/main/java/com/emf_asso/bdd/reseauetudiantsmuslmansdefrance/core/entity/Inscription.java
+++ app/src/main/java/com/emf_asso/bdd/reseauetudiantsmuslmansdefrance/core/entity/Inscription.java
@@ -1,7 +1,33 @@
 package com.emf_asso.bdd.reseauetudiantsmuslmansdefrance.core.entity;
 
+import java.io.Serializable;
+import java.util.Date;
+
 /**
  * Created by Omar_Desk on 10/11/2015.
  */
-public class Inscription {
+public class Inscription implements Serializable {
+
+    private UserMember user;
+    private Date registration_date;
+
+    public Inscription() {
+    }
+
+    public Date getRegistration_date() {
+        return registration_date;
+    }
+
+    public void setRegistration_date(Date registration_date) {
+        user.setRegistration_date(registration_date);
+        this.registration_date = registration_date;
+    }
+
+    public UserMember getUser() {
+        return user;
+    }
+
+    public void setUser(UserMember user) {
+        this.user = user;
+    }
 }

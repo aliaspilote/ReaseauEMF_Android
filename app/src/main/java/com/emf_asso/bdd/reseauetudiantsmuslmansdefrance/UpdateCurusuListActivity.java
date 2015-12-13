b9c4d0f20d70044348f8e0252ac181ac0476c665
tmerrika <taha.merrika@gmail.com>
2015-12-13 23:30:08 +0100

    profile

--- app/src/main/java/com/emf_asso/bdd/reseauetudiantsmuslmansdefrance/UpdateCurusuListActivity.java
+++ app/src/main/java/com/emf_asso/bdd/reseauetudiantsmuslmansdefrance/UpdateCurusuListActivity.java
@@ -1,4 +1,5 @@
 package com.emf_asso.bdd.reseauetudiantsmuslmansdefrance;
+
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
@@ -14,31 +15,23 @@ import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
-import android.view.ViewStub;
 import android.widget.ArrayAdapter;
 import android.widget.ImageView;
-import android.widget.LinearLayout;
 import android.widget.ListView;
-import android.widget.TextView;
 import android.widget.Toast;
 
-import com.emf_asso.bdd.reseauetudiantsmuslmansdefrance.core.entity.Curriculum;
-import com.emf_asso.bdd.reseauetudiantsmuslmansdefrance.core.entity.Skill;
 import com.emf_asso.bdd.reseauetudiantsmuslmansdefrance.core.entity.UserMember;
-import com.emf_asso.bdd.reseauetudiantsmuslmansdefrance.core.other.ListViewInit;
 import com.emf_asso.bdd.reseauetudiantsmuslmansdefrance.core.other.MenuDrawer;
 import com.emf_asso.bdd.reseauetudiantsmuslmansdefrance.core.other.Messages;
 import com.emf_asso.bdd.reseauetudiantsmuslmansdefrance.core.services.SessionWsService;
-
-import java.text.SimpleDateFormat;
 
 /**
  * Created by taha on 24/11/2015.
  */
 
-public class UserMemberProfilActivity extends AppCompatActivity {
+public class UpdateCurusuListActivity extends AppCompatActivity {
 
-    public int Current_Position = 0;
+    public int Current_Position = 5;
     public SessionWsService AppCtx;
     public MenuDrawer menu;
     private UserMember usermember;
@@ -49,6 +42,7 @@ public class UserMemberProfilActivity extends AppCompatActivity {
     private DrawerLayout mDrawerLayout;
     private String mActivityTitle;
     private ListView maListViewPerso;
+
     public UserMember getUsermember() {
         return usermember;
     }
@@ -60,9 +54,8 @@ public class UserMemberProfilActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_usermember_profil);
+        setContentView(R.layout.activity_update_listcursus);
         menu = new MenuDrawer(this, Current_Position);
-        InitStubs();
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayShowTitleEnabled(false);
@@ -79,7 +72,7 @@ public class UserMemberProfilActivity extends AppCompatActivity {
             SystemClock.sleep(3000);
             gotoMainActivity();
         }
-        ListViewInit.loadListStaticPI_View(this, AppCtx);
+        //ListViewInit.loadListStaticPI_View(this, AppCtx);
         setUsermember(AppCtx.getUserMember());
         mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
         menu.setMaListViewPerso(maListViewPerso);
@@ -92,8 +85,6 @@ public class UserMemberProfilActivity extends AppCompatActivity {
         mActivityTitle = getTitle().toString();
         setupDrawer();
         ImageListener();
-        CreateProfil();
-        fillInfoPerso();
         mDrawerLayout.openDrawer(GravityCompat.START);
     }
 
@@ -162,122 +153,13 @@ public class UserMemberProfilActivity extends AppCompatActivity {
         home_icon.setOnClickListener(new View.OnClickListener() {
             // Start new list activity
             public void onClick(View v) {
-                Intent mainIntent = new Intent(context, UserMemberProfilActivity.class);
+                Intent mainIntent = new Intent(context, UpdateCurusuListActivity.class);
                 startActivity(mainIntent);
             }
         });
 
     }
 
-    public void fillInfoPerso() {
-        SimpleDateFormat sdf = new SimpleDateFormat("dd-MM-yyy");
-        if (usermember.getName() != null)
-            fillInfoPersoByStep(usermember.getName(), R.id.editxt_ins_name);
-        if (usermember.getFirstname() != null)
-            fillInfoPersoByStep(usermember.getFirstname(), R.id.editxt_ins_firstname);
-        if (usermember.getBirth_date() != null)
-            fillInfoPersoByStep(sdf.format(usermember.getBirth_date()), R.id.editxt_ins_birthday);
-        if (usermember.getZip_code() != null)
-            fillInfoPersoByStep(usermember.getZip_code(), R.id.editxt_ins_zipcode);
-        if (usermember.getCity() != null)
-            fillInfoPersoByStep(usermember.getCity(), R.id.editxt_ins_city);
-        if (usermember.getPhone() != null)
-            fillInfoPersoByStep(usermember.getPhone(), R.id.editxt_ins_phone);
-    }
-
-    public void fillInfoPersoByStep(String value, int content) {
-        TextView textview = (TextView) findViewById(content);
-        textview.setText(value);
-    }
-
-    public void InitStubs() {
-        ((ViewStub) findViewById(R.id.stub_pup0)).inflate();
-        ((ViewStub) findViewById(R.id.stub_pup1)).inflate();
-        ((ViewStub) findViewById(R.id.stub_pup2)).inflate();
-        ((ViewStub) findViewById(R.id.stub_pup3)).inflate();
-        ((ViewStub) findViewById(R.id.stub_pup4)).inflate();
-
-        findViewById(R.id.stub_Inflated_pup1).setVisibility(View.GONE);
-        findViewById(R.id.stub_Inflated_pup2).setVisibility(View.GONE);
-        findViewById(R.id.stub_Inflated_pup3).setVisibility(View.GONE);
-        findViewById(R.id.stub_Inflated_pup4).setVisibility(View.GONE);
-    }
-
-    public void CreateProfil() {
-        SimpleDateFormat sdf = new SimpleDateFormat("dd-MM-yyy");
-
-        if (usermember.getRegistration_date() != null)
-            CreateProfilByStep("Date d'inscription :", sdf.format(usermember.getRegistration_date()), R.id.content_for_registrationdate);
-
-        if (usermember.getEmail() != null)
-            CreateProfilByStep("Email :", usermember.getEmail(), R.id.content_for_email);
-
-        if (usermember.getName() != null)
-            CreateProfilByStep("Nom :", usermember.getName(), R.id.content_for_name);
-
-        if (usermember.getFirstname() != null)
-            CreateProfilByStep("Prénom :", usermember.getFirstname(), R.id.content_for_firstname);
-
-        if (usermember.getBirth_date() != null)
-            CreateProfilByStep("Date de naissance :", sdf.format(usermember.getBirth_date()), R.id.content_for_birthday);
-
-        if (usermember.getZip_code() != null)
-            CreateProfilByStep("Code Postale :", usermember.getZip_code(), R.id.content_for_zipcode);
-
-        if (usermember.getCity() != null)
-            CreateProfilByStep("Ville :", usermember.getCity(), R.id.content_for_city);
-
-        if (usermember.getInvolvement() != null)
-            CreateProfilByStep("Engagement :", usermember.getInvolvement().toString(), R.id.content_for_involevment);
-
-        if (usermember.getSection() != null)
-            CreateProfilByStep("Section :", usermember.getSection().toString(), R.id.content_for_section);
-
-        if (usermember.getSkills() != null) {
-            String listString = "";
-            int i = 0;
-            for (Skill s : usermember.getSkills()) {
-                listString += s;
-                i++;
-                if (i < usermember.getSkills().size()) {
-                    listString += ",\n";
-                }
-            }
-            CreateProfilByStep("Compétences :", listString, R.id.content_for_skills);
-        }
-        // il faut mettre une liste
-        if (usermember.getStatus() != null)
-            CreateProfilByStep("Contact pour :", usermember.getStatus().toString(), R.id.content_for_contact);
-
-
-        if (!usermember.getCurriculum().isEmpty()) {
-            String listString = "";
-            int i = 0;
-            for (Curriculum s : usermember.getCurriculum()) {
-                listString += s;
-                i++;
-                if (i < usermember.getCurriculum().size()) {
-                    listString += ",\n";
-                }
-            }
-            CreateProfilByStep("Vos Cursus :", listString, R.id.content_for_curriculum);
-        }
-
-    }
-
-    public void CreateProfilByStep(String label, String value, int content) {
-        TextView Label = new TextView(this);
-        Label.setText(label);
-        TextView Value = new TextView(this);
-        Value.setText(value);
-
-        Label.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.FILL_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT, 1.0f));
-        Value.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.FILL_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT, 1.0f));
-
-        LinearLayout linearlayout = (LinearLayout) findViewById(content);
-        linearlayout.addView(Label);
-        linearlayout.addView(Value);
-    }
 
     public void DisplayToast(String text, int time) {
         if (time > 0)
--- app/src/main/java/com/emf_asso/bdd/reseauetudiantsmuslmansdefrance/core/other/MenuDrawer.java
+++ app/src/main/java/com/emf_asso/bdd/reseauetudiantsmuslmansdefrance/core/other/MenuDrawer.java
@@ -1,28 +1,168 @@
 package com.emf_asso.bdd.reseauetudiantsmuslmansdefrance.core.other;
 
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.view.ViewStub;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
 
 import com.emf_asso.bdd.reseauetudiantsmuslmansdefrance.R;
+import com.emf_asso.bdd.reseauetudiantsmuslmansdefrance.UpdateCurusuListActivity;
+import com.emf_asso.bdd.reseauetudiantsmuslmansdefrance.core.services.SessionWsService;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
 
 /**
  * Created by taha on 13/12/2015.
  */
 public class MenuDrawer extends AppCompatActivity {
-    public MenuDrawer() {
+    public Activity activity;
+    public int Current_Position;
+    Context context;
+    SessionWsService AppCtx;
+    private DrawerLayout mDrawerLayout;
+    private ListView maListViewPerso;
+
+    public MenuDrawer(Activity activity, int Current_Position) {
+        this.activity = activity;
+        this.Current_Position = Current_Position;
     }
 
-    public void InitStubs() {
-        ((ViewStub) findViewById(R.id.stub_pup0)).inflate();
-        ((ViewStub) findViewById(R.id.stub_pup1)).inflate();
-        ((ViewStub) findViewById(R.id.stub_pup2)).inflate();
-        ((ViewStub) findViewById(R.id.stub_pup3)).inflate();
-        ((ViewStub) findViewById(R.id.stub_pup4)).inflate();
-
-        findViewById(R.id.stub_Inflated_pup1).setVisibility(View.GONE);
-        findViewById(R.id.stub_Inflated_pup2).setVisibility(View.GONE);
-        findViewById(R.id.stub_Inflated_pup3).setVisibility(View.GONE);
-        findViewById(R.id.stub_Inflated_pup4).setVisibility(View.GONE);
+    public void setContext(Context context) {
+        this.context = context;
     }
+
+    public void setAppCtx(SessionWsService appCtx) {
+        AppCtx = appCtx;
+    }
+
+    public void setmDrawerLayout(DrawerLayout mDrawerLayout) {
+        this.mDrawerLayout = mDrawerLayout;
+    }
+
+    public void setMaListViewPerso(ListView maListViewPerso) {
+        this.maListViewPerso = maListViewPerso;
+    }
+
+    public void addDrawerItems() {
+
+        maListViewPerso = (ListView) this.activity.findViewById(R.id.navList);
+        ArrayList<HashMap<String, String>> listItem = new ArrayList<HashMap<String, String>>();
+        HashMap<String, String> map;
+        map = new HashMap<String, String>();
+        map.put("title", "Informations Générales");
+        map.put("img", String.valueOf(R.drawable.ic_info_perso));
+        listItem.add(map);
+        map = new HashMap<String, String>();
+        map.put("title", "Informations Personnelles");
+        map.put("img", String.valueOf(R.drawable.ic_info_perso));
+        listItem.add(map);
+        map = new HashMap<String, String>();
+        map.put("title", "Profil EMF");
+        map.put("img", String.valueOf(R.drawable.ic_profil_emf));
+        listItem.add(map);
+        map = new HashMap<String, String>();
+        map.put("title", "Compétences/Talents");
+        map.put("img", String.valueOf(R.drawable.ic_skills));
+        listItem.add(map);
+        map = new HashMap<String, String>();
+        map.put("title", "Changer mot de passe");
+        map.put("img", String.valueOf(R.drawable.ic_change_pwd));
+        listItem.add(map);
+        map = new HashMap<String, String>();
+        map.put("title", "Mes Cursus");
+        map.put("img", String.valueOf(R.drawable.ic_cursus));
+        listItem.add(map);
+        map = new HashMap<String, String>();
+        map.put("title", "Désactiver le compte");
+        map.put("img", String.valueOf(R.drawable.ic_disable_profil));
+        listItem.add(map);
+        map = new HashMap<String, String>();
+        map.put("title", "Déconnexion");
+        map.put("img", String.valueOf(R.drawable.ic_disconnect));
+        listItem.add(map);
+
+        SimpleAdapter mSchedule = new SimpleAdapter(activity.getBaseContext(), listItem, R.layout.listview_item,
+                new String[]{"img", "title"}, new int[]{R.id.img_menu_item, R.id.title_menu_item});
+        maListViewPerso.setAdapter(mSchedule);
+
+        maListViewPerso.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            @SuppressWarnings("unchecked")
+            public void onItemClick(AdapterView<?> a, View v, int position, long id) {
+                //HashMap<String, String> map = (HashMap<String, String>) maListViewPerso.getItemAtPosition(position);
+                MenuAction(position);
+                mDrawerLayout.closeDrawers();
+            }
+        });
+    }
+
+
+    public void hideStubByPosition(int position) {
+        switch (position) {
+            case 0:
+                this.activity.findViewById(R.id.stub_Inflated_pup0).setVisibility(View.GONE);
+                break;
+            case 1:
+                this.activity.findViewById(R.id.stub_Inflated_pup1).setVisibility(View.GONE);
+                break;
+            case 2:
+                this.activity.findViewById(R.id.stub_Inflated_pup2).setVisibility(View.GONE);
+                break;
+            case 3:
+                this.activity.findViewById(R.id.stub_Inflated_pup3).setVisibility(View.GONE);
+                break;
+            case 4:
+                this.activity.findViewById(R.id.stub_Inflated_pup4).setVisibility(View.GONE);
+                break;
+            case 5:
+                Intent intent = new Intent(context, UpdateCurusuListActivity.class);
+                // context.startActivity(intent);
+
+            default:
+
+                break;
+        }
+    }
+
+    public void displayStubByPosition(int position) {
+        switch (position) {
+            case 0:
+                this.activity.findViewById(R.id.stub_Inflated_pup0).setVisibility(View.VISIBLE);
+                break;
+            case 1:
+                this.activity.findViewById(R.id.stub_Inflated_pup1).setVisibility(View.VISIBLE);
+                break;
+            case 2:
+                this.activity.findViewById(R.id.stub_Inflated_pup2).setVisibility(View.VISIBLE);
+                break;
+            case 3:
+                this.activity.findViewById(R.id.stub_Inflated_pup3).setVisibility(View.VISIBLE);
+                break;
+            case 4:
+                this.activity.findViewById(R.id.stub_Inflated_pup4).setVisibility(View.VISIBLE);
+                break;
+
+            default:
+                // findViewById(R.id.stub_Inflated_pup1).setVisibility(View.VISIBLE);
+                break;
+        }
+    }
+
+    public void MenuAction(int position) {
+        hideStubByPosition(Current_Position);
+        displayStubByPosition(position);
+        Current_Position = position;
+    }
+
+
+
+
+
 }
